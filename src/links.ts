f40532f0c25101bb9c7ea0c3d70080c5d6afb61e
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-10-24 14:33:24 +0200

    SO-40: Change ConfirmAccount URL

--- src/links.ts
+++ src/links.ts
@@ -2,7 +2,7 @@
 export const SignInLink = "/";
 export const SignUpLink = "/registration";
 export const ResetPasswordConfirmationLink = "/reset-password-confirmation";
-export const ConfirmAccountLink = `/account-confirmation/:userId`;
+export const ConfirmAccountLink = "/account-confirmation/:userId";
 export const ResetPasswordLink = "/reset-password";
 export const ApprovedEmailLink = "/approved-email";
 export const ApprovedResetPasswordLink = "/approved-reset-password";
