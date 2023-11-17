42b7c7bbca00e59d5cacce6b9633f8912cc115ef
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-11-17 18:55:47 +0100

    SO-24: Add responsive layout module

--- src/components/Layout/ListItems.tsx
+++ src/components/Layout/ListItems.tsx
@@ -5,14 +5,15 @@ import ListItemText from "@mui/material/ListItemText";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 import { useNavigate } from "react-router-dom";
 import { MainPageLink } from "links";
+import classes from "./classes/Layout.module.css";
 
 export default function ListItems() {
   const navigate = useNavigate();
 
   return (
     <React.Fragment>
-      <ListItemButton onClick={() => navigate(MainPageLink)}>
-        <ListItemIcon>
+      <ListItemButton onClick={() => navigate(MainPageLink)} className={classes["layout__navigation__list__item"]}>
+        <ListItemIcon className={classes["layout__navigation__list__item__icon"]}>
           <CalendarMonthIcon />
         </ListItemIcon>
         <ListItemText primary="Kalendarz" />
