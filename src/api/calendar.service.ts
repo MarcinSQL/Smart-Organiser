b167a6b688f0cb5bf79d9b5d4ac9037d37a5c86f
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-12-11 21:16:24 +0100

    SO-53: Add get request to calendar events with name improvements to CreateCalendarEvents

--- src/api/calendar.service.ts
+++ src/api/calendar.service.ts
@@ -3,8 +3,14 @@ import { IModalEventsForm } from "modules/types/dashboard/calendar.types";
 
 const baseURL = "/calendar/";
 
-export const dashboardCreateEvent = (event: IModalEventsForm) => {
+export const dashboardCreateCalendarEvent = (event: IModalEventsForm) => {
   return service.post(`${baseURL}event/`, event).then((response) => {
     return Promise.resolve(response);
+  });
+};
+
+export const dashboardCalendarEvents = () => {
+  return service.get(`${baseURL}event/`).then((response) => {
+    return Promise.resolve(response.data);
   });
 };
