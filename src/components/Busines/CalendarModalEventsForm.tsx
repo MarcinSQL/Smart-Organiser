5da3fec4709126a3514a1c1880bd43228ead8434
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-12-17 15:54:00 +0100

    SO-60: Modify create event form schema

--- src/components/Busines/CalendarModalEventsForm.tsx
+++ src/components/Busines/CalendarModalEventsForm.tsx
@@ -8,21 +8,26 @@ import {
   RadioGroup,
   FormControlLabel,
   Radio,
+  Grid,
+  Checkbox,
 } from "@mui/material";
-
+import { DesktopTimePicker } from "@mui/x-date-pickers/DesktopTimePicker";
 import TextInput from "components/UI/TextInput";
 import classes from "../Pure/classes/ModalEvents.module.css";
 
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useCreateEventMutation } from "modules/logic/dashboard/mutations";
+import { useState } from "react";
 
 interface IFormInput {
-  startDate: string;
-  endDate: string;
   title: string;
+  day: string;
+  isAllDay: boolean;
+  startTime: string;
+  endTime: string;
+  eventType: string;
   note?: string;
-  type: string;
 }
 
 interface ICalendarModalEvents {
@@ -34,18 +39,27 @@ export default function CalendarModalEvents(props: ICalendarModalEvents) {
 
   const mutation = useCreateEventMutation();
 
+  const [currTime, setCurrTime] = useState(new Date().toLocaleTimeString());
+
+  const handleTimeChange = (props: string) => {
+    setCurrTime(props);
+  };
+
   let userSchema = yup.object().shape({
     title: yup.string().required("Tytuł jest wymagany"),
-    startDate: yup
+    day: yup
       .string()
       .required("Data rozpoczęcia wydarzenia jest wymagana")
       .default(defaultStartDate),
-    endDate: yup
+    isAllDay: yup.boolean(),
+    startTime: yup
       .string()
-      .required("Data zakończenia wydarzenia jest wymagana")
-      .default(defaultStartDate),
+      .required("Data rozpoczęcia wydarzenia jest wymagana"),
+    endTime: yup
+      .string()
+      .required("Data zakończenia wydarzenia jest wymagana"),
+    eventType: yup.string().required("Typ jest wymagany"),
     note: yup.string(),
-    type: yup.string().required("Typ jest wymagany"),
   });
 
   const { register, handleSubmit, control } = useForm<IFormInput>({
@@ -69,29 +83,52 @@ export default function CalendarModalEvents(props: ICalendarModalEvents) {
         label="Tytuł"
         {...register("title", { required: true })}
       />
-      <TextInput
-        control={control}
-        required
-        type="date"
-        label="Data rozpoczęcia"
-        defaultValue={defaultStartDate}
-        {...register("startDate", { required: true })}
-      />
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <TextInput
+            control={control}
+            required
+            type="date"
+            label="Data rozpoczęcia"
+            defaultValue={defaultStartDate}
+            {...register("day", { required: true })}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <FormControlLabel
+            label="Cały dzień:"
+            control={<Checkbox defaultChecked />}
+            {...register("isAllDay", { required: false })}
+          />
+        </Grid>
+      </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <DesktopTimePicker
+            label={"Godzina rozpoczęcia"}
+            ampm={false}
+            onChange={handleTimeChange}
+            value={currTime}
+            {...register("startTime", { required: false })}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DesktopTimePicker
+            label={"Godzina rozpoczęcia"}
+            ampm={false}
+            onChange={handleTimeChange}
+            value={currTime}
+            {...register("endTime", { required: false })}
+          />
+        </Grid>
+      </Grid>
       <TextInput
         control={control}
         required
         type="date"
         label="Data zakończenia"
         defaultValue={defaultStartDate}
-        {...register("endDate", { required: false })}
-      />
-      <TextInput
-        control={control}
-        label="Notatka (opcjonalnie)"
-        type="text"
-        multiline
-        rows={4}
-        {...register("note", { required: false })}
+        {...register("endTime", { required: true })}
       />
       <FormControl>
         <FormLabel id="event-type-label">Typ wydarzenia</FormLabel>
@@ -106,17 +143,25 @@ export default function CalendarModalEvents(props: ICalendarModalEvents) {
             value="private"
             control={<Radio />}
             label="Prywatne"
-            {...register("type", { required: true })}
+            {...register("eventType", { required: true })}
           />
           <FormControlLabel
             className={classes["form__radio-btn"]}
             value="business"
             control={<Radio />}
             label="Służbowe"
-            {...register("type", { required: true })}
+            {...register("eventType", { required: true })}
           />
         </RadioGroup>
       </FormControl>
+      <TextInput
+        control={control}
+        label="Notatka (opcjonalnie)"
+        type="text"
+        multiline
+        rows={4}
+        {...register("note", { required: false })}
+      />
       <Button
         type="submit"
         fullWidth
