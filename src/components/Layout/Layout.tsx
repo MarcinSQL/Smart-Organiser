cd65e31161623e39c4804442e2c6ac34e3574c89
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2023-12-26 18:50:46 +0100

    SO-55: Add working hamburger menu

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { CssBaseline, Box } from "@mui/material";
+import { CssBaseline, Box, Drawer } from "@mui/material";
 import LayoutHeader from "./LayoutHeader";
 import LayoutNavigation from "./LayoutNavigation";
 import { ILayout } from "modules/types/dashboard/layout.types";
@@ -7,15 +7,36 @@ import LayoutContent from "./LayoutContent";
 import classes from "./classes/Layout.module.css";
 import { useNavigate } from "react-router-dom";
 import { SignInLink } from "links";
+import { useState } from "react";
 
 export default function Layout(props: ILayout) {
   const navigate = useNavigate();
+  const [mobiNavOpen, setMobiNavOpen] = useState(false);
+
   if (!localStorage.getItem("token")) navigate(SignInLink);
+
+  const handleMobileNavigationOpen = (props: boolean) => {
+    setMobiNavOpen(props);
+  };
+
+  const handleClosingMobileNavigation = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if(event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')){
+      return;
+    }
+    setMobiNavOpen(open);
+  }
+
   return (
     <Box className={classes.layout}>
       <CssBaseline />
-      <LayoutHeader />
-      <LayoutNavigation />
+      <LayoutHeader mobiNav={handleMobileNavigationOpen} />
+      <Drawer
+        anchor="left"
+        open={mobiNavOpen}
+        onClose={handleClosingMobileNavigation(false)}
+      >
+        <LayoutNavigation />
+      </Drawer>
       <LayoutContent children={props.children} />
     </Box>
   );
