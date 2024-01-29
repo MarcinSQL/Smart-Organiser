1f5638fe2e09f9a4c11c6f205b786e1b9e4a674a
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2024-01-29 22:54:19 +0100

    SO-78: Implement toast to authorization views

--- src/modules/views/authorization/SignIn.tsx
+++ src/modules/views/authorization/SignIn.tsx
@@ -1,4 +1,3 @@
-import { useContext } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
@@ -11,13 +10,11 @@ import Copyright from "components/Copyright";
 import TextInput from "components/UI/TextInput";
 import { Avatar } from "@mui/material";
 import BlackboxStudioLogo from "assets/images/Blackbox_Studio_1.png";
-import AuthContext from "store/auth-context";
-import Toast from "../../../components/UI/Toast";
 import useSignIn from "modules/logic/authorization/useSignIn";
 import { logoHeight, logoWidth } from "components/utils/sizes";
+import { Toaster } from "react-hot-toast";
 
 export default function SignIn() {
-  const ctx = useContext(AuthContext);
   const {
     handleSubmit,
     register,
@@ -40,19 +37,20 @@ export default function SignIn() {
       }}
     >
       <CssBaseline />
+      <Toaster position={"bottom-right"} />
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          mb: 6
+          mb: 6,
         }}
       >
         <Avatar
           src={BlackboxStudioLogo}
           alt="Blackbox Studio logo"
           variant="rounded"
-          sx={{ width: logoWidth, height: logoHeight, m: 4}}
+          sx={{ width: logoWidth, height: logoHeight, m: 4 }}
         />
         <Typography component="h1" variant="h5">
           Zaloguj się
@@ -82,12 +80,20 @@ export default function SignIn() {
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link onClick={goToResetPassword} variant="body2" sx={{cursor: "pointer"}}>
+              <Link
+                onClick={goToResetPassword}
+                variant="body2"
+                sx={{ cursor: "pointer" }}
+              >
                 Zapomniałeś hasła?
               </Link>
             </Grid>
             <Grid item>
-              <Link onClick={goToSignUp} variant="body2" sx={{cursor: "pointer"}}>
+              <Link
+                onClick={goToSignUp}
+                variant="body2"
+                sx={{ cursor: "pointer" }}
+              >
                 Nie masz konta? Zarejestruj się
               </Link>
             </Grid>
@@ -95,7 +101,6 @@ export default function SignIn() {
         </Box>
       </Box>
       <Copyright />
-      {ctx.isError && <Toast message={ctx.message} />}
     </Container>
   );
 }
--- src/modules/views/authorization/SignUp.tsx
+++ src/modules/views/authorization/SignUp.tsx
@@ -1,5 +1,3 @@
-import { useContext } from "react";
-
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import Link from "@mui/material/Link";
@@ -12,13 +10,11 @@ import Copyright from "components/Copyright";
 import TextInput from "components/UI/TextInput";
 import { Avatar } from "@mui/material";
 import BlackboxStudioLogo from "assets/images/Blackbox_Studio_1.png";
-import Toast from "../../../components/UI/Toast";
-import AuthContext from "store/auth-context";
 import useSignUp from "modules/logic/authorization/useSignUp";
 import { logoHeight, logoWidth } from "components/utils/sizes";
+import { Toaster } from "react-hot-toast";
 
 export default function SignUp() {
-  const ctx = useContext(AuthContext);
   const { handleSubmit, register, onSubmit, control, goToSignIn } = useSignUp();
 
   return (
@@ -34,6 +30,7 @@ export default function SignUp() {
       }}
     >
       <CssBaseline />
+      <Toaster position={"bottom-right"} />
       <Box
         sx={{
           display: "flex",
@@ -51,7 +48,11 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Zarejestruj się
         </Typography>
-        <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1, textAlign: "center" }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ mt: 1, textAlign: "center" }}
+        >
           <Grid container spacing={2}>
             <Grid item xs={4}>
               <TextInput
@@ -87,17 +88,12 @@ export default function SignUp() {
           >
             Zarejestruj się
           </Button>
-          <Link
-            onClick={goToSignIn}
-            variant="body2"
-            sx={{cursor: "pointer"}}
-          >
+          <Link onClick={goToSignIn} variant="body2" sx={{ cursor: "pointer" }}>
             {"Posiadasz konto? Zaloguj się"}
           </Link>
         </Box>
       </Box>
       <Copyright />
-      {ctx.isError && <Toast message={ctx.message} />}
     </Container>
   );
 }
