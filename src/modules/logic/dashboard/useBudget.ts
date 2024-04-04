a071409aea3e6ad3edeaaea86a257b8fd70213a5
Marcin Wasilewski <marcin.wasilewski512@gmail.com>
2024-04-04 21:23:19 +0200

    SO-91: Add working budget navigation

--- src/modules/logic/dashboard/useBudget.ts
+++ src/modules/logic/dashboard/useBudget.ts
@@ -1,8 +1,93 @@
 import { useEffect, useState } from "react";
 import { useGetCostMutation } from "./mutations";
+import dayjs from "dayjs";
+import { useGetCostsQuery } from "./queries";
+import { IBudgetCosts } from "modules/types/dashboard/budget.types";
 
 export default function useBudget() {
+  const [displayedDate, setDisplayedDate] = useState(dayjs());
+  displayedDate.locale("pl");
+
   const mutation = useGetCostMutation();
+
+  // WHEN ENDPOINTS WILL BE CREATED
+  //const { data } = useGetCostsQuery();
+
+  const TEMPdata = [
+    {
+      id: "0",
+      title: "kcnxjvnxivx",
+      amount: 222,
+      description: "Typical keyboard",
+      type: "expenses",
+      date: "2024-03-12T12:53:36",
+      category: "entertainment",
+    },
+    {
+      id: "1",
+      title: "jgfjgdj",
+      amount: 53,
+      description: "Thgfd hdhg",
+      type: "expenses",
+      date: "2024-03-12T12:53:36",
+      category: "entertainment",
+    },
+    {
+      id: "2",
+      title: "dasdada",
+      amount: 42,
+      description: " hg hdf hg hf",
+      type: "expenses",
+      date: "2024-03-12T12:53:36",
+      category: "entertainment",
+    },
+
+    {
+      id: "3",
+      title: "Keyboard",
+      amount: 222,
+      description: "Typical keyboard",
+      type: "expenses",
+      date: "2024-04-12T12:53:36",
+      category: "entertainment",
+    },
+    {
+      id: "4",
+      title: "Mouse",
+      amount: 222,
+      description: "Typical keyboard",
+      type: "expenses",
+      date: "2024-05-12T12:53:36",
+      category: "entertainment",
+    },
+    {
+      id: "5",
+      title: "Asdasda",
+      amount: 222,
+      description: "Typical keyboard",
+      type: "expenses",
+      date: "2024-06-12T12:53:36",
+      category: "entertainment",
+    },
+    {
+      id: "6",
+      title: "DDDDDDDDDDDDDD",
+      amount: 222,
+      description: "Typical keyboard",
+      type: "expenses",
+      date: "2024-07-12T12:53:36",
+      category: "entertainment",
+    },
+  ];
+
+  const rowsList: IBudgetCosts[] = [];
+
+  TEMPdata?.forEach((element: IBudgetCosts) => {
+    if (element.date.slice(5, 7) == displayedDate.format("MM")) {
+      rowsList.push(element);
+    }
+  });
+
   const [showModalChoose, setShowModalChoose] = useState(false);
 
   const [showModalCost, setShowModalCost] = useState(false);
@@ -39,9 +124,13 @@ export default function useBudget() {
     }
   }, [mutation.isSuccess]);
 
-  const handleMonthPrev = () => {};
+  const handleMonthPrev = () => {
+    setDisplayedDate(displayedDate.subtract(1, "month"));
+  };
 
-  const handleMonthNext = () => {};
+  const handleMonthNext = () => {
+    setDisplayedDate(displayedDate.subtract(-1, "month"));
+  };
 
   const handleModalChooseOpen = () => {
     setShowModalChoose(true);
@@ -83,5 +172,7 @@ export default function useBudget() {
     handleModalCostOpen,
     handleModalCostClose,
     showModalCost,
+    rowsList,
+    displayedDate,
   };
 }
